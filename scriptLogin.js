ff5d6d788954b903c4fd3a351973f71e1c0a0dbd
aadriiii22 <aadriiii22>
2025-11-24 14:35:35 +0100

    accion profe, comentada

--- scriptLogin.js
+++ scriptLogin.js
@@ -8,3 +8,9 @@ function go() {
     alert("Por favor ingrese nombre de usuario y contraseña correctos");
   }
 }
+
+//como lo ha hecho el profe, se insertaría desde la línea 5
+// location.assign("main.html"); //<- Nombre de la ruta
+//} else {
+// aler("Porfavor ingrese, nombre de usuario y contraseña ...")
+//}
